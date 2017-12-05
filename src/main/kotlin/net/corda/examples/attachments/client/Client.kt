ef8e8de155f7dfdc105e7f1b5e0183e2bab44dce
joeldudleyr3 <joel.dudley@r3.com>
2017-12-05 17:51:27 +0000

    Adds example of downloading and checking the JAR's contents via RPC.

--- src/main/kotlin/net/corda/examples/attachments/client/Client.kt
+++ src/main/kotlin/net/corda/examples/attachments/client/Client.kt
@@ -1,11 +1,16 @@
 package net.corda.examples.attachments.client
 
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.crypto.SecureHash
+import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.utilities.NetworkHostAndPort.Companion.parse
 import net.corda.core.utilities.loggerFor
+import net.corda.examples.attachments.ATTACHMENT_EXPECTED_CONTENTS
+import net.corda.examples.attachments.ATTACHMENT_FILE_NAME
 import net.corda.examples.attachments.BLACKLIST_JAR_PATH
 import org.slf4j.Logger
 import java.io.File
+import java.util.jar.JarInputStream
 
 /**
  * Uploads the jar of blacklisted counterparties with whom agreements cannot be struck to the node.
@@ -26,12 +31,46 @@ private class UploadBlacklistClient {
             val rpcConnection = CordaRPCClient(nodeAddress).start("user1", "test")
             val proxy = rpcConnection.proxy
 
-            val attachmentInputStream = File(BLACKLIST_JAR_PATH).inputStream()
-            proxy.uploadAttachment(attachmentInputStream)
-
+            val attachmentHash = uploadAttachment(proxy, BLACKLIST_JAR_PATH)
             logger.info("Blacklist uploaded to node at $nodeAddress")
+
+            val attachmentJar = downloadAttachment(proxy, attachmentHash)
+            logger.info("Blacklist downloaded from node at $nodeAddress")
+
+            checkAttachment(attachmentJar, ATTACHMENT_FILE_NAME, ATTACHMENT_EXPECTED_CONTENTS)
+            logger.info("Attachment contents checked on node at $nodeAddress")
 
             rpcConnection.notifyServerAndClose()
         }
+    }
+}
+
+/**
+ * Uploads the attachment at [attachmentPath] to the node.
+ */
+private fun uploadAttachment(proxy: CordaRPCOps, attachmentPath: String): SecureHash {
+    val attachmentUploadInputStream = File(attachmentPath).inputStream()
+    return proxy.uploadAttachment(attachmentUploadInputStream)
+}
+
+/**
+ * Downloads the attachment with hash [attachmentHash] from the node.
+ */
+private fun downloadAttachment(proxy: CordaRPCOps, attachmentHash: SecureHash): JarInputStream {
+    val attachmentDownloadInputStream = proxy.openAttachment(attachmentHash)
+    return JarInputStream(attachmentDownloadInputStream)
+}
+
+/**
+ * Checks the [expectedFileName] and [expectedContents] of the downloaded [attachmentJar].
+ */
+private fun checkAttachment(attachmentJar: JarInputStream, expectedFileName: String, expectedContents: List<String>) {
+    while (attachmentJar.nextEntry.name != expectedFileName) {
+        attachmentJar.nextEntry
+    }
+    val contents = attachmentJar.bufferedReader().readLines()
+
+    if (contents != expectedContents) {
+        throw IllegalArgumentException("Downloaded JAR did not have the expected contents.")
     }
 }
